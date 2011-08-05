3973ba9501eec4a4c4fbfdfbca1c95fd171541de
Fletcher Nichol <fnichol@nichol.ca>
2011-08-05 11:57:25 -0600

    Freaking rdoc README default.

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures user"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
