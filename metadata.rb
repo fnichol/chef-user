479b0a419d2f9d702f5cfc9f9c4a3867674f3ec3
Fletcher Nichol <fnichol@nichol.ca>
2011-08-12 21:30:23 -0600

    Update metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,9 @@
 maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
-description      "Installs/Configures user"
+description      "A convenient Chef LWRP to manage user accounts and SSH keys"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "user", "This recipe is a no-op and does nothing."
+recipe "user::data_bag", "Processes a list of users with data drawn from a data bag."
