e17419773bb7a420c847d04771c62aeddc4d0dbc
Fletcher Nichol <fnichol@nichol.ca>
2012-04-25 22:43:51 -0600

    Test refactorings.

--- test/resources/account_spec.rb
+++ test/resources/account_spec.rb
@@ -1,29 +1,8 @@
 require 'minitest/autorun'
-require 'chef/resource'
-
-module Mixins
-  def load_resource(cookbook, lrwp)
-    Chef::Resource.build_from_file(cookbook.to_s, File.expand_path(File.join(
-      File.dirname(__FILE__), %w{.. .. resources}, "#{lwrp.to_s}.rb")), nil)
-  end
-
-  def unload_resource(cookbook, lwrp)
-    Chef::Resource.send(:remove_const, lwrp_const(cookbook, lwrp))
-  end
-
-  def resource_klass(cookbook, lwrp)
-    Chef::Resource.const_get(lwrp_const(cookbook, lrwp))
-  end
-
-  private
-
-  def lwrp_const(cookbook, lwrp)
-  :"#{cookbook.to_s.capitalize}#{lwrp.to_s.capitalize}"
-  end
-end
+require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe 'Chef::Resource::UserAccount' do
-  include Mixins
+  include ResourceMixins
 
   let(:cookbook)    { :user }
   let(:lwrp)        { :account }
