ea58de961ba4edd9a3d5ff055f547bb32d45ef3d
Ranjib Dey <ranjib@pagerduty.com>
2015-03-22 11:10:32 -0700

    new release

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,16 @@
-name             "user"
-maintainer       "Fletcher Nichol"
-maintainer_email "fnichol@nichol.ca"
-license          "Apache 2.0"
-description      "A convenient Chef LWRP to manage user accounts and SSH keys (this is not the opscode users cookbook)"
+name             'user'
+maintainer       'Fletcher Nichol'
+maintainer_email 'fnichol@nichol.ca'
+license          'Apache 2.0'
+description      'A convenient Chef LWRP to manage user accounts and SSH keys (this is not the opscode users cookbook)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          '0.4.0'
 
-supports "ubuntu"
-supports "debian"
-supports "mac_os_x"
-supports "suse"
-supports "omnios"
+supports 'ubuntu'
+supports 'debian'
+supports 'mac_os_x'
+supports 'suse'
+supports 'omnios'
 
-recipe "user", "This recipe is a no-op and does nothing."
-recipe "user::data_bag", "Processes a list of users with data drawn from a data bag."
+recipe 'user', 'This recipe is a no-op and does nothing.'
+recipe 'user::data_bag', 'Processes a list of users with data drawn from a data bag.'
