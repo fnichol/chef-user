56e7fb1c27fbc18a5e15fa10c47815bb3738d34d
Tim Heckman <t@heckman.io>
2016-11-19 18:13:45 -0800

    version bump to prepare for v0.6.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fnichol@nichol.ca'
 license          'Apache 2.0'
 description      'A convenient Chef LWRP to manage user accounts and SSH keys (this is not the opscode users cookbook)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.1'
+version          '0.6.0'
 
 issues_url 'https://github.com/fnichol/chef-user/issues'
 source_url 'https://github.com/fnichol/chef-user'
